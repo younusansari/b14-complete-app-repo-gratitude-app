02ed7c007afb872d7bb66ab526ae012c5089d56c
younusansari <younus.ansari82@gmail.com>
2026-03-01 16:09:49 +0530

    Added ec2 instance files

---
+++ infra/EC2/main.tf
@@ -0,0 +1,44 @@
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get default subnet
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = tolist(data.aws_subnet_ids.default.ids)[0]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
---
+++ infra/EC2/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region    = "us-west-1"
+instance_type = "t2.micro"
+key_name      = "Yunus-Key"
+#ami_id        = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (example)
+ami_id        = "ami-0290e60ec230db1e4"  # Amazon Ubuntu (example)
