fd399f0027a8d99cec7653672f33b9de5068c485
Prashant Dey <prashantsavior@gmail.com>
2026-03-01 16:13:01 +0530

    WIP: adding the ec2

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,12 @@ module "website_bucket" {
   bucket_name = var.website_bucket_name
 
 }
+
+module "app_ec2" {
+  source             = "./modules/ec2_instance"
+  ami_id             = var.ec2_ami_id
+  instance_type      = var.ec2_instance_type
+  subnet_id          = var.ec2_subnet_id
+  security_group_ids = var.ec2_security_group_ids
+  key_name           = var.ec2_key_name
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -8,6 +8,39 @@ variable "aws_region" {
   type        = string
 }
 
+variable "ec2_ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ec2_subnet_id" {
+  description = "Subnet ID for EC2 instance"
+  type        = string
+}
+
+variable "ec2_security_group_ids" {
+  description = "Security group IDs for EC2 instance"
+  type        = list(string)
+}
+
+variable "ec2_key_name" {
+  description = "Optional key pair name for EC2"
+  type        = string
+  default     = null
+}
+
+variable "ec2_tags" {
+  description = "Tags for EC2 instance"
+  type        = map(string)
+  default     = {}
+}
+
 # variable "aws_profile" {
 #   description = "AWS profile"
 #   type        = string
